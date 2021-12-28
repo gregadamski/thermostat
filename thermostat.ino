5102e68b91bf6c89905f84bfdc5d44f2acd65868
Greg Adamski <greg.adamski@gmail.com>
2021-12-28 14:33:27 +0100

    Added display + temp readouts

--- thermostat.ino
+++ thermostat.ino
@@ -2,15 +2,108 @@
 
 byte type_s;
 byte addr[8];
+OneWire  ds(D5);  // on pin 10 (a 4.7K resistor is necessary)
 
+#define H1PIN D6
+#define H2PIN D7
+
+// display initialization
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+float targetTemp = 70.0;
+float histeresis = 1.0;
+float heating = false;
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // initialize the temp sensor
   pinMode(D5, INPUT_PULLUP);     // Initialize the LED_BUILTIN pin as an output
+  delay(250);
+  initTempSensor();
 
+  // heater relay init
+  //initHeaterPins();
+  initDisplay();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float temp;
+  temp = measureTemp();
+  Serial.println(temp);
+  
+  boolean heater1 = false;
+  boolean heater2 = false;
+
+  String heater1StatusStr = String("H1: ") + String(heater1) + String("; H2: ") + String(heater2);
+  String tempStatusStr = String("Temp: ") +  String(temp);
+  printStatus(heater1StatusStr, tempStatusStr, F(""));
+
+  delay(1000);
+  
+}
+
+void initHeaterPins()
+{
+    pinMode(H1PIN, OUTPUT);
+    pinMode(H2PIN, OUTPUT);
+}
+
+
+void setHeaterState(int heater, boolean enabled)
+{
+
+  int heaterState = enabled ? HIGH : LOW;
+  int heaterPin;
+  if (heater == 1) {
+    heaterPin = H1PIN;    
+  } else
+  if (heater == 2) {
+    heaterPin = H1PIN;    
+  } else {
+    return;
+  }
+
+  digitalWrite(heaterPin, heaterState);
+}
+
+
+void initDisplay()
+{
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.display();
+
+}
+
+void printStatus(String line1, String line2, String line3) {
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+
+
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(line1);
+  display.println(line2);
+  display.println(line3);
+
+  display.display();
 }
 
 
@@ -78,22 +171,17 @@ float measureTemp()
   byte data[12];
   float celsius;
 
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
 
   present = ds.reset();
   ds.select(addr);    
   ds.write(0xBE);         // Read Scratchpad
 
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
   for ( i = 0; i < 9; i++) {           // we need 9 bytes
     data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
   }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
 
   // Convert the data to actual temperature
   // because the result is a 16 bit signed integer, it should
@@ -116,9 +204,4 @@ float measureTemp()
   }
   celsius = (float)raw / 16.0;
   return celsius;
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
